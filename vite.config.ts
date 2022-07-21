b391fa920f4a1bf2ac628c87fc79863cab65ea08
zuowendong <17326195691@163.com>
2022-07-21 16:50:16 +0800

    feat: 类型文件打包

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
 
+// vite
+import dts from "vite-plugin-dts";
+
 // postcss
 import postcssMixins from "postcss-mixins";
 import postcssRem from "postcss-rem";
@@ -12,7 +15,14 @@ import Delete from "rollup-plugin-delete";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        dts({
+            outputDir: "dist",
+            staticImport: true,
+            insertTypesEntry: true,
+        }),
+        vue(),
+    ],
     build: {
         lib: {
             entry: path.resolve(__dirname, "src/index.ts"),
