9ea5b8acc1a5dc81433b42e3b8a0de837e64ecc6
zuowendong <17326195691@163.com>
2022-10-08 21:09:34 +0800

    feat: update .gitignore in site

--- site/.vuepress/.temp/internal/pagesComponents.js
+++
@@ -1,8 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export const pagesComponents = {
-  // path: /
-  "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"D:/Code/zyfui/site/.vuepress/.temp/pages/index.html.vue")),
-  // path: /404.html
-  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/Code/zyfui/site/.vuepress/.temp/pages/404.html.vue")),
-}
--- site/.vuepress/.temp/internal/pagesRoutes.js
+++
@@ -1,4 +0,0 @@
-export const pagesRoutes = [
-  ["v-8daa1a0e","/",{"title":"zyf UI 组件库"},["/index.html","/README.md"]],
-  ["v-3706649a","/404.html",{"title":""},["/404"]],
-]
