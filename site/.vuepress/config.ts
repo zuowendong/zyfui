7cc6938cc99fcccccbf01813fc654b775c72ccc5
zuowendong <17326195691@163.com>
2022-10-14 10:40:08 +0800

    feat: add workflows

--- site/.vuepress/config.ts
+++ site/.vuepress/config.ts
@@ -1,7 +1,27 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig, defaultTheme, viteBundler } from "vuepress";
+import { resolve } from "path";
 
+console.log(process.env)
 export default defineUserConfig({
-    lang: 'zh-CN',
-    title: 'zyf UI',
-    description: '这是我的第一个 VuePress 站点',
-})
+    base: process.env.NODE_ENV === 'development' ? '/' : '/zyfui/',
+	lang: "zh-CN",
+	title: "zyf UI",
+	description: "这是我的第一个 VuePress 站点",
+	locales: {
+		"/": {
+			lang: "zh-CN",
+			title: "zyfui",
+		},
+	},
+	theme: defaultTheme({}),
+	bundler: viteBundler({
+		viteOptions: {
+			resolve: {
+				alias: {
+					zyfui: resolve(__dirname, "../../packages/zyfui/src/index.ts"),
+				},
+			},
+		},
+	}),
+    plugins: []
+});
